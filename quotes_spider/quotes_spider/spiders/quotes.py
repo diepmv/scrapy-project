9247b9a683fff8edab8536b7fed1474527116a42
Mai Diep <diepmv@vccloud.vn>
2018-08-27 15:26:09 +0700

    add login

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import FormRequest
+from scrapy.loader import ItemLoader
+from scrapy.utils.response import open_in_browser
 
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    # allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/login']
 
     def parse(self, response):
+        token = response.xpath('//*[@name="csrf_token"]/@value').extract_first()
+        return FormRequest.from_response(response,
+                                         formdata={'csrf_token': token, 'username': 'foo', 'password': 'foo'},
+                                         callback=self.scrape_home_page)
+
+    def scrape_home_page(self, response):
         #h1_tag = response.xpath('//h1/a/text()').extract_first()
         #tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
         #yield {'H1 Tag': h1_tag, 'Tags': tags}
+        open_in_browser(response)
+
         quotes = response.xpath('//*[@class="quote"]')
         for quote in quotes:
             text = quote.xpath('.//*[@class="text"]/text()').extract_first()
