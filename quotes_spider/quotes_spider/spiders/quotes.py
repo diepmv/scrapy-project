747126a62fb6ab2168fe55a8e7a005fb3c0fafcb
Mai Diep <diepmv@vccloud.vn>
2018-08-27 10:06:35 +0700

    complete advance spider

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -8,6 +8,18 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        h1_tag = response.xpath('//h1/a/text()').extract_first()
-        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
-        yield {'H1 Tag': h1_tag, 'Tags': tags}
+        #h1_tag = response.xpath('//h1/a/text()').extract_first()
+        #tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        #yield {'H1 Tag': h1_tag, 'Tags': tags}
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+            author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
+            tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
+
+            yield {'Text': text, 'Author': author, 'Tags': tags}
+
+        next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        # yield scrapy.Request(absolute_next_page_url, callback='parse')
+        yield scrapy.Request(absolute_next_page_url)
